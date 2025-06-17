aff46d6d478c4eed264c16d215d5a5960171f16f
Pratham Patil <129663114+PraTham-Patill@users.noreply.github.com>
2025-06-17 18:22:06 +0530

    UserDTO.java

--- src/main/java/com/fintrack/personalfinancemanager/dto/UserDTO.java
+++ src/main/java/com/fintrack/personalfinancemanager/dto/UserDTO.java
@@ -5,10 +5,10 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Pattern;
-import javax.validation.constraints.Size;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 
 /**
  * Data Transfer Object for User entity.
